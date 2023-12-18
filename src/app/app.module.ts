bc255d0791a9f2548c8e2cc60e5491089c4a588e
jsdev2016 km <youramigohere@gmail.com>
2023-12-18 16:03:10 +0000

    second commit - component Course Card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CourseCardComponent } from './course-card/course-card.component';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule, CourseCardComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/course-card/course-card.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'course-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './course-card.component.html',
+  styleUrl: './course-card.component.css'
+})
+export class CourseCardComponent {
+
+}
